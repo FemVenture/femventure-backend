6ad356c279c6e4bfba807b49a5a7f50e0b7cc97c
TMedalith <u20211f955@upc.edu.pe>
2024-11-08 22:33:35 -0500

    feat(milestones): enhance Milestone DTOs with additional fields and data types

--- src/main/java/com/femventure/ProjectManagement/domain/dto/Project/request/MilestoneRequestDto.java
+++ src/main/java/com/femventure/ProjectManagement/domain/dto/Project/request/MilestoneRequestDto.java
@@ -3,6 +3,7 @@ package com.femventure.ProjectManagement.domain.dto.Project.request;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.query.sql.internal.ParameterRecognizerImpl;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -13,7 +14,12 @@ import java.time.LocalDate;
 public class MilestoneRequestDto {
     private String title;
     private String description;
+    private LocalDate completionDate;
+    private String status;
+    private String evidence;
     private LocalDate startDate;
     private LocalDate endDate;
-    private BigDecimal fundingGoal;
+    private double fundingGoal;
+    private double fundsRaised;
+
 }
